d4d0e8708008e85bb08b426733327460308a9dda
Kutman <ipomea875@gmail.com>
2026-07-04 13:19:02 +0600

    Add Patient model class with encapsulated fields

---
+++ src/models/Patient.java
@@ -0,0 +1,79 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Patient {
+    private String id;
+    private String name;
+    private LocalDate dob;
+    private String type;
+    private String symptoms;
+    private String extraField;
+    private LocalDate date;
+    private LocalTime time;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSymptoms() {
+        return symptoms;
+    }
+
+    public void setSymptoms(String symptoms) {
+        this.symptoms = symptoms;
+    }
+
+    public String getExtraField() {
+        return extraField;
+    }
+
+    public void setExtraField(String extraField) {
+        this.extraField = extraField;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+}
